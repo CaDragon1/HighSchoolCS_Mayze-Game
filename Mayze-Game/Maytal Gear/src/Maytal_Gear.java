6433c7720ec0abbc35d409266980345d4d0cfdfe
Drake.Caleb.s581300 <s581300@rrisd.local>
2017-05-01 10:59:56 -0500

    First push.

---
+++ Mayze-Game/Maytal Gear/src/Maytal_Gear.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Maytal_Gear.java
+ *
+ * Maytal_Gear application
+ *
+ * @author 
+ * @version 1.00 2017/5/1
+ */
+ 
+public class Maytal_Gear {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}

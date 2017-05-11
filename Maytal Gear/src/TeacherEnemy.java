cf6e9ad547981b7427231165f5611026a7e96896
Drake.Caleb.s581300 <s581300@rrisd.local>
2017-05-11 11:34:35 -0500

    update

---
+++ Maytal Gear/src/TeacherEnemy.java
@@ -0,0 +1,21 @@
+import mayflower.*;
+/**
+ * @(#)TeacherEnemy.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/5/11
+ */
+
+
+public class TeacherEnemy extends Actor{
+
+    public TeacherEnemy() {
+    	setImage("img/Batmanman.png");
+    }
+    
+    @Override
+    public void act(){
+    }
+    
+}
